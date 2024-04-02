5b17bc9921fc9dd18b9599adb13ccb320c2c28a3
Franco Silva <franco_tigres18@hotmail.com>
2024-04-02 13:20:24 -0600

    feat:(Documentación Crear Producto)

--- src/router.ts
+++ src/router.ts
@@ -95,6 +95,35 @@ router.get(
   getProductById
 );
 
+/**
+ * @swagger
+ * /api/products:
+ *  post:
+ *    summary: Creates a new Product
+ *    tags:
+ *      - Products
+ *    description: Returns a new record in the database
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              name: 
+ *                type: string
+ *                example: "Tablet 12 pulgadas"
+ *              price:
+ *                type: number
+ *                example: 399
+ *    responses:
+ *      201:
+ *        description: Product created succesfully
+ *      400:
+ *        description: Bad Request - Invalid inpu data 
+ *  
+*/
+
 router.post(
   '/',
   // Validación
