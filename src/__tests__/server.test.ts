4f53d4eba1c9f0ebfa6cd2fcdc52691a644abff5
Franco Silva <franco_tigres18@hotmail.com>
2024-04-02 08:16:08 -0600

    feat:(Test Actualizar disponibilidad de un producto)

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -11,6 +11,7 @@
 import request from 'supertest';
 import server from '../server';
 
+
 describe('GET /api', () => {
   it('should send back a json response', async () => {
     const res = await request(server).get('/api');
--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -2,32 +2,24 @@ import { Request, Response } from 'express';
 import Product from '../models/Product.model';
 
 export const getProducts = async (req: Request, res: Response) => {
-  try {
-    const products = await Product.findAll({
-      order: [['id', 'DESC']],
-      // limit: 2
-      // *Excluir atributos de la consulta
-      // attributes: { exclude: ['createdAt', 'updatedAt', 'availability'] },
-    });
-    res.json({ data: products });
-  } catch (error) {
-    console.log(error);
-  }
+  const products = await Product.findAll({
+    order: [['id', 'DESC']],
+    // limit: 2
+    // *Excluir atributos de la consulta
+    // attributes: { exclude: ['createdAt', 'updatedAt', 'availability'] },
+  });
+  res.json({ data: products });
 };
 
 export const getProductById = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByPk(id);
+const { id } = req.params;
+const product = await Product.findByPk(id);
 
-    if (!product) {
-      return res.status(404).json({ error: 'Producto no encontrado' });
-    }
+if (!product) {
+  return res.status(404).json({ error: 'Producto no encontrado' });
+}
 
-    res.json({ data: product });
-  } catch (error) {
-    console.log(error);
-  }
+res.json({ data: product });
 };
 
 export const createProduct = async (req: Request, res: Response) => {
@@ -36,12 +28,8 @@ export const createProduct = async (req: Request, res: Response) => {
   // const savedProduct = await product.save();
 
   // * Forma 2 - Guardar
-  try {
-    const product = await Product.create(req.body);
-    res.status(201).json({ data: product });
-  } catch (error) {
-    console.log(error);
-  }
+  const product = await Product.create(req.body);
+  res.status(201).json({ data: product });
 };
 
 export const updateProduct = async (req: Request, res: Response) => {
